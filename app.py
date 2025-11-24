06c045b6a65649baea4a1cdc194340224d348dc9
imtinge <imtinge@163.com>
2025-11-24 17:14:33 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,28 +1,87 @@
+import os
+import sys
+import click
+from pathlib import Path
 from flask import Flask, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String, select, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from markupsafe import escape
 
-app = Flask(__name__)
 
-name = "imtinge"
-movies = [
-    {"title": "My Neighbor Totoro", "year": "1988"},
-    {"title": "Dead Poets Society", "year": "1989"},
-    {"title": "A Perfect World", "year": "1993"},
-    {"title": "Leon", "year": "1994"},
-    {"title": "Mahjong", "year": "1996"},
-    {"title": "Swallowtail Butterfly", "year": "1996"},
-    {"title": "King of Comedy", "year": "1999"},
-    {"title": "Devils on the Doorstep", "year": "1999"},
-    {"title": "WALL-E", "year": "2008"},
-    {"title": "The Pork of Music", "year": "2012"},
-]
+SQLITE_PREFIX = "sqlite:///" if sys.platform.startswith("win") else "sqlite:////"
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLITE_PREFIX + str(
+    Path(app.root_path) / "data.db"
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(app, model_class=Base)
+
+
+class User(db.Model):
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(20))
+
+
+class Movie(db.Model):
+    __tablename__ = "movie"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(60))
+    year: Mapped[str] = mapped_column(String(4))
+
+
+@app.cli.command("init-db")
+@click.option("--drop", is_flag=True, help="Create after drop.")
+def init_database(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("Initialized database.")
+
+
+@app.cli.command()
+def forge():
+    db.drop_all()
+    db.create_all()
+
+    name = "imtinge"
+    movies = [
+        {"title": "My Neighbor Totoro", "year": "1988"},
+        {"title": "Dead Poets Society", "year": "1989"},
+        {"title": "A Perfect World", "year": "1993"},
+        {"title": "Leon", "year": "1994"},
+        {"title": "Mahjong", "year": "1996"},
+        {"title": "Swallowtail Butterfly", "year": "1996"},
+        {"title": "King of Comedy", "year": "1999"},
+        {"title": "Devils on the Doorstep", "year": "1999"},
+        {"title": "WALL-E", "year": "2008"},
+        {"title": "The Pork of Music", "year": "2012"},
+    ]
+    user = User(name=name)
+    db.session.add(user)
+
+    for m in movies:
+        movie = Movie(title=m["title"], year=m["year"])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo("Done")
 
 
 @app.route("/")
 @app.route("/home")
 @app.route("/index")
 def hello():
-    return render_template("index.html", name=name, movies=movies)
+    user = db.session.scalar(select(User))
+    movies = db.session.scalars(select(Movie)).all()
+    return render_template("index.html", user=user, movies=movies)
 
 
 @app.route("/user/<name>")
